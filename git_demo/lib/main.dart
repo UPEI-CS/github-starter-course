3b46325f8c705cbe2c8f4257c39f2d3ff901e1d5
Andrew Godbout <agodbout@upei.ca>
2022-01-05 09:06:13 -0400

    change wording for number of button taps text

--- git_demo/lib/main.dart
+++ git_demo/lib/main.dart
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have tapped the button this many times:',
+              'You have  tapped the button this many times:',
             ),
             Text(
               '$_counter',
